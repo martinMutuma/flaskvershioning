197d234d55f91ca4f6c3e083c127dd9cd5572cf5
Martin Mutuma <smartimutuma@gmail.com>
2019-01-27 20:52:25 +0300

    Update Ci Config

--- common.py
+++ common.py
@@ -1,7 +1,11 @@
 #mainpath/common.py
 #file Used to run the app
 from app import app, db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     app.run(debug=True)
