d041327b60b2afc67a2a9fe5d052f0bd6394fee3
Martin Mutuma <smartimutuma@gmail.com>
2019-01-27 20:58:59 +0300

    Update Ci Config

--- common.py
+++ common.py
@@ -7,5 +7,10 @@ from flask_migrate import Migrate, MigrateCommand
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def create_db():
+    """Creates the db tables."""
+    db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
